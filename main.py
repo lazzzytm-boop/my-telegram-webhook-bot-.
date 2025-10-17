44abd2b38fb5c3d0eac95f5294d8531eaf302bfe
lazzzytm-boop <lazzzytm@gmail.com>
2025-10-17 23:40:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -72,8 +72,9 @@ if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        # ... (Ваша логика остановки)
+        
         pass
         logger.warning("Bot stopped by KeyboardInterrupt")
+
 
 
