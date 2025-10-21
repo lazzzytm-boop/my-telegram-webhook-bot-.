00e2da4f1599069794eab32873a1764677d32ae1
lazzzytm-boop <lazzzytm@gmail.com>
2025-10-21 23:03:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -53,17 +53,20 @@ dp.startup.register(DataBase.on_startup)
 dp.startup.register(on_startup) 
 # Хэндлер, который будет принимать обновления (ЭТО НУЖНО ДОБАВИТЬ)
 app.router.add_post(f'/{BOT_TOKEN}', dp.web_handler) 
+runner = web.AppRunner(app)
+await runner.setup()
+# *** ОБЯЗАТЕЛЬНО: указываем порт 8080 ***
+site = web.TCPSite(runner, host='0.0.0.0', port=8080) 
+await site.start() 
+
 if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
         pass
 # # 4. Запуск веб-сервера
-runner = web.AppRunner(app)
-await runner.setup()
-# *** ОБЯЗАТЕЛЬНО: указываем порт 8080 ***
-site = web.TCPSite(runner, host='0.0.0.0', port=8080) 
-await site.start() 
+
+
 
 
 
