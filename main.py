4da4b27b49ce189b8ccc418b2ef2d7ff487409ca
lazzzytm-boop <lazzzytm@gmail.com>
2025-10-21 22:42:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -38,16 +38,16 @@ async def main():
     
     logger.info("Starting bot in Webhook mode...")
  # # Инициализация (ДОБАВИТЬ ЭТО)
-    dp = Dispatcher(bot)
-    bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
-   app = web.Application() # <- Создание веб-приложения AIOHTTP                                                                                                                                                          
-    # Инициализация
-    dp = Dispatcher()
-    bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
-    
-    # Регистрация роутеров и базы данных (как было)
-    dp.include_routers(client_router, admin_router)
-    dp.startup.register(DataBase.on_startup)
+dp = Dispatcher(bot)
+bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
+app = web.Application() # <- Создание веб-приложения AIOHTTP                                                                                                                                                          
+# Инициализация
+dp = Dispatcher()
+bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
+
+# Регистрация роутеров и базы данных (как было)
+dp.include_routers(client_router, admin_router)
+dp.startup.register(DataBase.on_startup)
     
     # 1. Регистрация функции установки Webhook
     dp.startup.register(on_startup) 
@@ -64,12 +64,6 @@ await runner.setup()
 site = web.TCPSite(runner, host='0.0.0.0', port=8080) 
 await site.start() 
 
-
-
-
-
-# ...
-
 if __name__ == '__main__':
     try:
         asyncio.run(main())
@@ -77,6 +71,7 @@ if __name__ == '__main__':
         
         pass
         logger.warning("Bot stopped by KeyboardInterrupt")
+
 
 
 
